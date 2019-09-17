90ecb630c3825f59e218cd7a8bb1b6c9b38e5086
farhadyasir <iraz2426@gmail.com>
2019-09-17 17:20:00 +1000

    setup basic hydration

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import BasicTodos from './todos/BasicTodos';
 
-import { api } from '../api';
-import { useServerData } from '../state/serverDataContext';
+const style = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+};
 
 const Home = () => {
-  const serverTodos = useServerData(data => {
-    return data.todos || [];
-  });
-  const [text, setText] = useState('');
-  const [todos, setTodos] = useState(serverTodos);
-
   return (
     <div>
       <h1>Home page</h1>
-
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-
-          const newTodo = {
-            text
-          };
-
-          api.todos.create(newTodo).then(res => {
-            setTodos([...todos, res]);
-            setText('');
-          });
-        }}
-      >
-        <label htmlFor="todo">Add a todo</label>
-        <br />
-        <input
-          id="todo"
-          type="text"
-          value={text}
-          autoComplete="off"
-          onChange={e => setText(e.target.value)}
-        />
-      </form>
-
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
+      <div style={style}>
+        <BasicTodos /> 
+      </div>
     </div>
   );
-};
-
-Home.fetchData = () => {
-  return api.todos.all().then(todos => {
-    return {
-      todos
-    };
-  });
 };
 
 export default Home;
